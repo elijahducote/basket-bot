9f63909aae8ce680d4c74e3d39c1e2e25aaa5ff0
elijahducote <validpupflower@gmail.com>
2018-07-10 16:49:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready', async () => {
 
   client.channels.get('433451627282956292').send('<@226906826061512704>');
 
