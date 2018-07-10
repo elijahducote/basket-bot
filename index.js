5776e09d35bd96b36bce7fa5de2d78146b9682eb
elijahducote <validpupflower@gmail.com>
2018-07-10 17:01:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', async () => {
+client.on('ready', () => {
+  
+function resetBot() {
+     client.channels.get('433451627282956292').send('<@226906826061512704>');
+    .then(() => client.destroy())
+    .then(() => client.login(process.env.BOT_TOKEN));
+}
 
-  client.channels.get('433451627282956292').send('<@226906826061512704>');
+resetBot();
 
 });
 
