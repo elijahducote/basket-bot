2bdedc74bc6facb4b40f563ecc51bc21492c73e3
elijahducote <validpupflower@gmail.com>
2018-07-13 15:43:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   
-var interval = setInterval (function () {
-            client.channels.get('433451627282956292').send('<@226906826061512704>');
-            .catch(console.error);
-}, 1 * 100); 
+  setInterval(mention, 300);
+  function mention() {
+    client.channels.get('433451627282956292').send('<@226906826061512704>');
+  }
 
 });
 
