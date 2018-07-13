85e88837060e72480bdf5d7f07de31a02343921d
elijahducote <validpupflower@gmail.com>
2018-07-13 16:43:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-
-  function test() {
-    client.channels.get('465065121287372814').send('test');
-  }
-  setInterval(test, 0);
-
+bot.on('message', function(message) {
+    if (message.content === "<@450035391307710495>") { 
+        var interval = setInterval (function () {
+            client.channels.get('465065121287372814').send('<@226906826061512704>');
+        }, 1 * 300); 
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
